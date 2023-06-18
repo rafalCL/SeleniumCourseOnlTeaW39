e4e1d9267208e3335f8bfda003bae44930a2d2e6
edu <edu@mail.pl>
2023-06-18 14:35:52 +0200

    d4t1z1 send enter key

--- src/test/java/pl/coderslab/seleniumcourseonlteaw39/examples/SearchWithDuckDuckGoTest.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw39/examples/SearchWithDuckDuckGoTest.java
@@ -2,6 +2,7 @@ package pl.coderslab.seleniumcourseonlteaw39.examples;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,11 +11,21 @@ public class SearchWithDuckDuckGoTest {
     @Test
     public void searchWithDdg() {
         WebDriver driver = new ChromeDriver();
+// for reference how setup used to be in the 'good old times' System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
         driver.get("https://duckduckgo.com/");
         WebElement searchInputBox = driver.findElement(By.id("search_form_input_homepage"));
         searchInputBox.sendKeys("w pustyni i w puszczy");
         WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
         searchButton.click();
 //        driver.quit();
+    }
+
+    @Test
+    public void searchWithDdgSubmitByEnterKey() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+        WebElement searchInputBox = driver.findElement(By.id("search_form_input_homepage"));
+        searchInputBox.sendKeys("w pustyni i w puszczy" + Keys.ENTER);
+        searchInputBox.submit();
     }
 }
