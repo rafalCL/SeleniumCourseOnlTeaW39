d011f71889acd1ff1376db372e9ecc23baeb8238
edu <edu@mail.pl>
2023-06-18 16:49:36 +0200

    d4t3z1 hotel create account part 3 add assertion to confirm account creation

--- src/test/java/pl/coderslab/seleniumcourseonlteaw39/HotelRegisterUserTest.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw39/HotelRegisterUserTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 // Zadanie 1
 //Funkcje
 //Będąc na stronie rejestracji nowego użytkownika
@@ -26,8 +29,9 @@ public class HotelRegisterUserTest {
     public void selectAndFillElements() {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
-
+        // given
         driver.get("https://hotel-testlab.coderslab.pl/en/");
+        // when
         WebElement signInButton = driver.findElement(By.cssSelector("a.user_login.navigation-link"));
         signInButton.click();
 
@@ -54,5 +58,10 @@ public class HotelRegisterUserTest {
 
         WebElement registerButton  = driver.findElement(By.id("submitAccount"));
         registerButton.click();
+        // then
+        WebElement accountCreationSuccessfulPanel = driver.findElement(By.cssSelector("p.alert.alert-success"));
+        assertTrue(accountCreationSuccessfulPanel.isDisplayed());
+        String panelText = accountCreationSuccessfulPanel.getText();
+        assertEquals("Your account has been created.", panelText);
     }
 }
