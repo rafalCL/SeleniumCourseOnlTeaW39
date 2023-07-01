c75aa843755068cf5610a3c1a83b7499a2ba532e
edu <edu@mail.pl>
2023-07-01 14:26:26 +0200

    d5t2 refactor MainPage to use PageFactory to initialize WebElements

--- src/test/java/pl/coderslab/seleniumcourseonlteaw39/pageobjectpattern/pages/MainPage.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw39/pageobjectpattern/pages/MainPage.java
@@ -1,18 +1,19 @@
 package pl.coderslab.seleniumcourseonlteaw39.pageobjectpattern.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class MainPage {
-    private final WebDriver driver;
+    @FindBy(css = "a.user_login.navigation-link")
+    private WebElement signInButton;
 
     public MainPage(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void clickSignIn() {
-        WebElement signInButton = driver.findElement(By.cssSelector("a.user_login.navigation-link"));
         signInButton.click();
     }
 }
