a7addd93a0054c938f1bb78e1b5733c0e4c2b1d6
edu <edu@mail.pl>
2023-07-01 14:40:04 +0200

    d5t2 refactor AuthenticationPage and MyAccountPage to use PageFactory

--- src/test/java/pl/coderslab/seleniumcourseonlteaw39/pageobjectpattern/pages/AuthenticationPage.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw39/pageobjectpattern/pages/AuthenticationPage.java
@@ -3,19 +3,22 @@ package pl.coderslab.seleniumcourseonlteaw39.pageobjectpattern.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AuthenticationPage {
-    private final WebDriver driver;
+    @FindBy(id = "email_create")
+    WebElement emailInputBox;
+    @FindBy(id = "SubmitCreate")
+    WebElement createAnAccountButton;
 
     public AuthenticationPage(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void provideEmailAndClickCreateAnAccount(String email) {
-        WebElement emailInputBox = driver.findElement(By.id("email_create"));
         emailInputBox.clear();
         emailInputBox.sendKeys(email);
-        WebElement createAnAccountButton = driver.findElement(By.id("SubmitCreate"));
         createAnAccountButton.click();
     }
 }
--- src/test/java/pl/coderslab/seleniumcourseonlteaw39/pageobjectpattern/pages/MyAccountPage.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw39/pageobjectpattern/pages/MyAccountPage.java
@@ -1,18 +1,19 @@
 package pl.coderslab.seleniumcourseonlteaw39.pageobjectpattern.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class MyAccountPage {
-    private final WebDriver driver;
+    @FindBy(css = "p.alert.alert-success")
+    WebElement accountCreationSuccessfulPanel;
 
     public MyAccountPage(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public boolean isDisplayedAccountCreationSuccessPanel() {
-        WebElement accountCreationSuccessfulPanel = driver.findElement(By.cssSelector("p.alert.alert-success"));
         String panelText = accountCreationSuccessfulPanel.getText();
 
         return accountCreationSuccessfulPanel.isDisplayed() &&
