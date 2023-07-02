31ea78c03cd53014173880cbc77866bddcdc0ee8
edu <edu@mail.pl>
2023-07-02 12:28:56 +0200

    d6t2 cucumber example search with DDG part 5 - implement step check results

--- src/test/java/pl/coderslab/seleniumcourseonlteaw39/cucumber/SearchWithDuckDuckGoCucumberSteps.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw39/cucumber/SearchWithDuckDuckGoCucumberSteps.java
@@ -2,22 +2,29 @@ package pl.coderslab.seleniumcourseonlteaw39.cucumber;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pl.coderslab.seleniumcourseonlteaw39.pageobjectpattern.ddg.DdgMainPage;
+import pl.coderslab.seleniumcourseonlteaw39.pageobjectpattern.ddg.DdgSearchResultsPage;
 
 import java.time.Duration;
+import java.util.List;
+
+import static org.junit.Assert.fail;
 
 public class SearchWithDuckDuckGoCucumberSteps {
     private WebDriver driver;
     private DdgMainPage mainPage;
+    private DdgSearchResultsPage resultsPage;
 
     @Given("https:\\/\\/duckduckgo.com\\/ opened in web browser")
     public void openInBrowser() {
         this.driver = new ChromeDriver();
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
         this.mainPage = new DdgMainPage(driver);
+        this.resultsPage = new DdgSearchResultsPage(driver);
 
         driver.get("https://duckduckgo.com/");
     }
@@ -33,37 +40,16 @@ public class SearchWithDuckDuckGoCucumberSteps {
     public void clickSearchButton() {
         mainPage.clickSearchButton();
     }
-    //Then First 3 results contain phrase 'Faraon'
 
-    //     @Test
-    //    public void searchPhraseWithDuckDuckGo() {
-    //        // given
-    //        driver.get("https://duckduckgo.com/");
-    //
-    //        final String phrase = "faraon";
-    //        // when
-    //        mainPage.enterSearchPhrase(phrase);
-    //        mainPage.clickSearchButton();
-    //        // then
-    //        List<String> searchResults = resultsPage.getSearchResults();
-    //        for (int i = 0; i < 3; i++) {
-    //            String resultLowerCase = searchResults.get(i).toLowerCase();
-    //            if(!resultLowerCase.contains(phrase)) {
-    //                fail(String.format("Phrase %s NOT found in result: %s", phrase, resultLowerCase));
-    //            }
-    //        }
-    //    }
-    //
-    //    @BeforeEach
-    //    public void beforeEach() {
-    //        this.driver = new ChromeDriver();
-    //        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
-    //        this.mainPage = new DdgMainPage(driver);
-    //        this.resultsPage = new DdgSearchResultsPage(driver);
-    //    }
-    //
-    //    @AfterEach
-    //    public void afterEach() {
-    ////        this.driver.quit(); // todo uncomment to close browser and driver
-    //    }
+    @Then("First 3 results contain phrase 'Faraon'")
+    public void checkSearchResults() {
+        String phrase = "faraon";
+        List<String> searchResults = resultsPage.getSearchResults();
+        for (int i = 0; i < 3; i++) {
+            String resultLowerCase = searchResults.get(i).toLowerCase();
+            if(!resultLowerCase.contains(phrase)) {
+                fail(String.format("Phrase %s NOT found in result: %s", phrase, resultLowerCase));
+            }
+        }
+    }
 }
